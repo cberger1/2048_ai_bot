d938cadcaf7702a93fe7cee510915e3452bcb1c6
cberger1 <b5.xb360@gmail.com>
2019-01-04 10:33:22 +0100

    Add files via upload

--- 2048GameV2.py
+++ 2048GameV2.py
@@ -1,18 +1,17 @@
 import random
 import time
-
-gameData = [[],[]]
-data = []
+import numpy as  np
 
 class _2048_:
 
     def __init__(self):
         self.moves = {"right": 0, "up": 1, "left": 2, "down": 3}
+        self.nextMove = None
         self.highestNum = 2
-        self.board = [[0,0,0,0],
-                      [0,0,0,0],
-                      [0,0,0,0],
-                      [0,0,0,0]]
+        self.board = np.array([[0,0,0,0],
+                               [0,0,0,0],
+                               [0,0,0,0],
+                               [0,0,0,0]])
 
     def printBoard(self):
         for i in range(4):
@@ -32,89 +31,48 @@ class _2048_:
         else:
             self.board[x][y] = 2
 
-    def moveEveryNumTo(self, mv):
-        nextElement = 1
+    def moveEverythingToTheRight(self):
         for i in range(3):
-            columns = list(range(3 - i))
-            if mv == 2:
-                nextElement = -1
-                columns = list(range(3, 0 + i, -1))
             for row in range(4):
-                for column in columns:
+                for column in range(3 - i):
                     #move everything to the right
                     if self.board[row][column] != 0:
                         #if the next spot is empty: replace it
-                        if self.board[row][column + nextElement] == 0:
-                            self.board[row][column + nextElement] = self.board[row][column]
+                        if self.board[row][column + 1] == 0:
+                            self.board[row][column + 1] = self.board[row][column]
                             self.board[row][column] = 0
 
-    def moveEveryNumToUoD(self, mv):
-            nextElement = 1
-            for i in range(3):
-                columns = list(range(3 - i))
-                if mv == 3:
-                    nextElement = -1
-                    columns = list(range(3, 0 + i, -1))
-                for row in range(4):
-                    for column in columns:
-                        #move everything to the right
-                        if self.board[row][column] != 0:
-                            #if the next spot is empty: replace it
-                            if self.board[column + nextElement][row] == 0:
-                                self.board[column + nextElement][row] = self.board[column][row]
-                                self.board[column][row] = 0
-
-    def combineNumTo(self, mv):
-        columns = list(range(3))
-        rows = list(range(4))
-        nextElement = 1
-        if mv == 0:
-            columns.reverse()
-            nextElement = -1
-
-        for row in rows:
-            for column in columns:
+    def combineNumToTheRight(self):
+        #form right to left
+        for row in range(3,-1,-1):
+            for column in range(3,0,-1):
                 #combine
-                if self.board[row][column] == self.board[row][column + nextElement]:
+                if self.board[row][column] == self.board[row][column - 1]:
                     self.board[row][column] = self.board[row][column] * 2
-                    self.board[row][column + nextElement] = 0
+                    self.board[row][column -1] = 0
                     if self.highestNum < self.board[row][column]:
                         self.highestNum = self.board[row][column]
 
-    def combineNumToUoD(self, mv):
-        columns = list(range(3))
-        rows = list(range(4))
-        nextElement = 1
-        if mv == 1:
-            columns.reverse()
-            nextElement = -1
-
-        for row in rows:
-            for column in columns:
-                #combine
-                if self.board[column][row] == self.board[column + nextElement][row]:
-                    self.board[column][row] = self.board[column][row] * 2
-                    self.board[column + nextElement][row] = 0
-                    if self.highestNum < self.board[column][row]:
-                        self.highestNum = self.board[column][row]
+    def moveRight(self):
+        self.moveEverythingToTheRight()
+        self.combineNumToTheRight()
+        self.moveEverythingToTheRight()
 
     def move(self, to):
         if to == 0:
-            self.moveEveryNumTo(to)
-            self.combineNumTo(to)
-            self.moveEveryNumTo(to)
+            self.moveRight()
         elif to == 1:
-            self.moveEveryNumToUoD(to)
-            self.combineNumToUoD(to)
-            self.moveEveryNumToUoD(to)
+            self.board = np.rot90(self.board, 3)
+            self.moveRight()
+            self.board = np.rot90(self.board)
         elif to == 2:
-            self.moveEveryNumTo(to)
-            self.combineNumTo(to)
-            self.moveEveryNumTo(to)
+            self.board = np.fliplr(self.board)
+            self.moveRight()
+            self.board = np.fliplr(self.board)
         elif to == 3:
-            self.moveEveryNumToUoD(to)
-            self.combineNumToUoD(to)
-            self.moveEveryNumToUoD(to)
+            self.board = np.rot90(self.board)
+            self.moveRight()
+            self.board = np.rot90(self.board, 3)
         else:
             print("ERROR: to must beequl to 0/1/2/3 (0 := right; 1 := up; 2 := left; 3 := down)")
             print()
@@ -123,45 +81,38 @@ class _2048_:
         self.newNum()
         self.newNum()
 
-##    def isMovePossible(board, m):
-##        movePoss = True
-##        nb = move(board, m)
-##        if board.all() == nb.all():
-##            movePoss = False
-##        return movePoss
-##
-##    def noPossibleMove(board):
-##        noPossMove = False
-##        if np.min(board) != 0:
-##            possMoveCount = 0
-##            for i in range(4):
-##                if isMovePossible(board, i):
-##                    possMoveCount += 1
-##            if possMoveCount == 0:
-##                noPossMove = True
-##        return noPossMove
-
     def randomPlay(self):
-        self.move(2)
+        self.nextMove = random.randint(0,3)
+        self.move(self.nextMove)
         self.newNum()
 
-startTimer =time.clock()
+data = [[],[]]
+goal = 32
+maxMoveCountToReachGoal = 20
+gameSamples = 10000
 
-for j in range(1):
-    highestNum = 2
+startTimer = time.clock()
+for j in range(gameSamples):
     b = _2048_()
     b.startGame()
-    for i in range(20):
+    boardData = []
+    moveData= []
+    #boardData.append(b.board)
+    for i in range(maxMoveCountToReachGoal):
+        boardToAppend = b.board.tolist()
+        boardData.append(np.array(boardToAppend))
         b.randomPlay()
-        b.printBoard()
-        if highestNum == 32:
+        moveData.append(b.nextMove)
+        if b.highestNum == goal:
+            for k in range(i + 1):
+                data[0].append(boardData[k])
+                data[1].append(moveData[k])
             break
-    if j % 1000 == 0:
+    if j % 1000 == 0 and j != 0:
         print(j)
 
 stopTimer = time.clock()
 
 print("Time: ", stopTimer - startTimer)
-#print("Games who reached 32: ", len(stats))
-#if len(stats) != 0:
-    #print("Average move count: ", round(sum(stats) / len(stats), 2))
+print("Total boards: ", len(data[0]))
+
